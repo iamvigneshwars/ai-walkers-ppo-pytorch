c3e440b43209bf70374f937b5661c7aa11078a3c
iamvigneshwars <iamvigneshwars@gmail.com>
2022-03-11 22:02:23 +0000

    update

--- agent.py
+++ agent.py
@@ -1,4 +1,7 @@
 import gym
+from gym.wrappers import Monitor
+import time
+import os
 import pybulletgym
 import torch
 from model import PPO
@@ -6,13 +9,14 @@ from torch.distributions import Normal
 import torch.optim as optim
 from tensorboardX import SummaryWriter
 import numpy as np
+from multiprocessing_env import SubprocVecEnv
 import argparse
 
 class Agent:
     def __init__(self, environment, device):
         self.env_id = environment
         self.device = device
-        self.writter = SummaryWriter()
+        self.writer = SummaryWriter()
         self.log_probs = []
         self.values = []
         self.states = []
@@ -51,10 +55,9 @@ class Agent:
 
 
     def learn(self):
-        envs = gym.vector.SyncVectorEnv([self.make_env() for i in range(args.n_workers)])
-        num_inputs = envs.observation_space.shape[1]
-        num_outputs = envs.action_space[0].shape[0]
-
+        envs = [self.make_env() for i in range(args.n_workers)]
+        envs = SubprocVecEnv(envs)
+        env = gym.make(self.env_id)
         model = PPO(num_inputs, num_outputs).to(self.device)
         optimizer = optim.Adam(model.parameters(), lr = args.lr)
 
@@ -63,92 +66,145 @@ class Agent:
         best_reward = None
 
         state = envs.reset()
-        early_stop = None
+        early_stop = False
 
-        for _ in range(args.ppo_steps):
-            state = torch.FloatTensor(state).to(device)
-            dist, value = model(state)
-            action = dist.sample()
-            next_state, reward, done, _ = envs.step(action.cpu().numpy())
-            log_prob = dist.log_prob(action)
-            
-            self.log_probs.append(log_prob)
-            self.values.append(value)
-            self.rewards.append(torch.tensor(reward, dtype = torch.float32).unsqueeze(1).to(self.device))
-            self.masks.append(torch.tensor(1 - done).unsqueeze(1).to(self.device))
-            self.states.append(state)
-            self.actions.append(action)
+        while not early_stop:
 
-            state = next_state
-            frame_idx += 1
-
-        next_state = torch.FloatTensor(next_state).to(device)
-        _, next_value = model(next_state)
-        returns = self.calculate_gae(next_value, args.gamma, args.lmda)
-
-        returns = torch.cat(returns).detach()
-        log_probs = torch.cat(self.log_probs).detach()
-        values = torch.cat(self.values).detach()
-        states = torch.cat(self.states)
-        actions = torch.cat(self.actions)
-        advantage = returns - values
-        advantage = self.normalize(advantage)
-
-        count_steps = 0
-        sum_returns = 0
-        sum_advantage = 0.0
-        sum_loss_actor = 0.0
-        sum_loss_critic = 0.0
-        sum_entropy = 0.0
-        sum_loss_total = 0.0
-
-        for _ in range(args.epochs):
-            for state, action, old_log_probs, retn, adv in self.sample_batch(states, actions, log_probs, returns, advantage):
-
+            for _ in range(args.ppo_steps):
+                state = torch.FloatTensor(state).to(device)
                 dist, value = model(state)
-                entropy = dist.entropy().mean()
-                new_log_probs = dist.log_prob(action)
-                ratio = (new_log_probs - old_log_probs).exp()
-                surr1 = ratio * adv
-                surr2 = torch.clamp(ratio, 1.0- args.epsilon, 1.0 + args.epsilon) * adv
+                action = dist.sample()
+                next_state, reward, done, _ = envs.step(action.cpu().numpy())
+                log_prob = dist.log_prob(action)
+                
+                self.log_probs.append(log_prob)
+                self.values.append(value)
+                self.rewards.append(torch.tensor(reward, dtype = torch.float32).unsqueeze(1).to(self.device))
+                self.masks.append(torch.tensor(1 - done).unsqueeze(1).to(self.device))
+                self.states.append(state)
+                self.actions.append(action)
 
-                actor_loss = - torch.min(surr1, surr2).mean()
-                critic_loss = (retn  - value).pow(2).mean()
+                state = next_state
+                frame_idx += 1
 
-                total_loss = args.c1  * critic_loss + actor_loss - args.c2 * entropy
+            next_state = torch.FloatTensor(next_state).to(device)
+            _, next_value = model(next_state)
+            returns = self.calculate_gae(next_value, args.gamma, args.lmda)
 
-                optimizer.zero_grad()
-                total_loss.backward()
-                optimizer.step()
+            returns = torch.cat(returns).detach()
+            log_probs = torch.cat(self.log_probs).detach()
+            values = torch.cat(self.values).detach()
+            states = torch.cat(self.states)
+            actions = torch.cat(self.actions)
+            advantage = returns - values
+            advantage = self.normalize(advantage)
 
+            count_steps = 0
+            sum_returns = 0
+            sum_advantage = 0.0
+            sum_loss_actor = 0.0
+            sum_loss_critic = 0.0
+            sum_entropy = 0.0
+            sum_loss_total = 0.0
 
+            for _ in range(args.epochs):
+                for st, actn, old_log_probs, retn, adv in \
+                        self.sample_batch(states, actions, log_probs, returns, advantage):
+
+                    dist, value = model(st)
+                    entropy = dist.entropy().mean()
+                    new_log_probs = dist.log_prob(actn)
+                    ratio = (new_log_probs - old_log_probs).exp()
+                    surr1 = ratio * adv
+                    surr2 = torch.clamp(ratio, 1.0- args.epsilon, 1.0 + args.epsilon) * adv
+                    actor_loss = - torch.min(surr1, surr2).mean()
+                    critic_loss = (retn  - value).pow(2).mean()
+                    total_loss = args.c1  * critic_loss + actor_loss - args.c2 * entropy
+
+                    optimizer.zero_grad()
+                    total_loss.backward()
+                    optimizer.step()
+
+                    sum_returns += retn.mean()
+                    sum_advantage += adv.mean()
+                    sum_loss_actor += actor_loss
+                    sum_loss_critic += critic_loss
+                    sum_loss_total += total_loss
+                    sum_entropy += entropy
+
+                    count_steps += 1
+
+            self.writer.add_scalar("returns", sum_returns / count_steps, frame_idx)
+            self.writer.add_scalar("advantage", sum_advantage / count_steps, frame_idx)
+            self.writer.add_scalar("loss_actor", sum_loss_actor / count_steps, frame_idx)
+            self.writer.add_scalar("loss_critic", sum_loss_critic / count_steps, frame_idx)
+            self.writer.add_scalar("entropy", sum_entropy / count_steps, frame_idx)
+            self.writer.add_scalar("loss_total", sum_loss_total / count_steps, frame_idx)
+
+            train_epoch += 1
+            self.states.clear()
+            self.actions.clear()
+            self.rewards.clear()
+            self.masks.clear()
+            self.log_probs.clear()
+            self.values.clear()
+
+            if train_epoch % TEST_EPOCHS == 0:
+                test_reward = np.mean([self.play(env, model) for _ in range(10)])
+                self.writer.add_scalar('test_rewards', test_reward, frame_idx)
+                print('Frame %s. reward : %s ' % (frame_idx, test_reward))
+                if best_reward is None or best_reward <= test_reward:
+                    if best_reward is not None:
+                        print("Best reward updated : %.3f -> %.3f" % (best_reward, test_reward))
+                        name = "%s_best_%+.3f_%d.pth" %(self.env_id, test_reward, frame_idx)
+                        fname = os.path.join('.', 'checkpoints', name)
+                        torch.save(model.state_dict(), fname)
+                    best_reward = test_reward
+            
+    
+    def play(self,env = None, model = None, human = False):
+
+        if not env:
+            env = Monitor(gym.make(self.env_id), './video', force = True)
+
+        if not model:
+            model = PPO(env.observation_space.shape[0], env.action_space.shape[0]).to(self.device)
+            model.load_state_dict(torch.load(args.model))
         
+        if human:
+            env.render('human')
+
+        state = env.reset()
+        done= False
+        total_reward = 0
+
+        # while not done:
+        for _ in range(10000):
+            time.sleep(0.01)
+            state = torch.FloatTensor(state).unsqueeze(0).to(device)
+            dist, _ = model(state)
+            action = dist.sample().cpu().numpy()[0]
+            next_state, reward, done, _ = env.step(action)
+            state = next_state
+            total_reward += reward
+            if (human):
+                print("***SCORE :", total_reward,"***")
+        return total_reward
 
         
 if __name__ == "__main__":
-    
-    HIDDEN_SIZE         = 256
-    LEARNING_RATE       = 1e-4
-    GAMMA               = 0.99
-    GAE_LAMBDA          = 0.95
-    PPO_EPSILON         = 0.2
-    CRITIC_DISCOUNT     = 0.5
-    ENTROPY_BETA        = 0.001
-    PPO_STEPS           = 256
-    MINI_BATCH_SIZE     = 64
-    PPO_EPOCHS          = 10
-    TEST_EPOCHS         = 10
-    NUM_TESTS           = 10
-    TARGET_REWARD       = 2500
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", help = "OpenAI gym environment", default = "HalfCheetahPyBulletEnv-v0", type = str)
+    parser.add_argument("--learn", help = "Agent starts to learn",  action= 'store_true')
+    parser.add_argument("--play", help = "Agent starts to play", action= 'store_true')
     parser.add_argument("-n_workers", help = "Number of environments", default = 8, type = int)
     parser.add_argument("-mini_batch", help = "Size of mini batch to sample", default = 64, type = int)
     parser.add_argument("-lr", help = "Model learning rate", default = 1e-4, type = float)
     parser.add_argument("-gamma", help = "return discount factor", default = 0.99, type = float)
     parser.add_argument("-lmda", help = "gae lambda", default = 0.95, type = float)
     parser.add_argument("-epochs", help = "number of updates", default = 10, type = int)
+    parser.add_argument("-model", help = "pretrained model", type = str)
     parser.add_argument("-ppo_steps", help = "Number of steps before update", default = 256, type = int)
     parser.add_argument("-c1", help = "critic discount", default = 0.5, type = float)
     parser.add_argument("-c2", help = "entropy beta", default = 0.001, type = float)
@@ -158,5 +214,9 @@ if __name__ == "__main__":
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     agent = Agent(args.env, device)
-    agent.learn()
+
+    if (args.learn):
+        agent.learn()
+    if (args.play):
+        agent.play(human = True)
     
