0af6acd16da2efe0e95bc091bbb6e5cb630dc0b2
iamvigneshwars <iamvigneshwars@gmail.com>
2022-03-11 17:20:43 +0000

    initial commit

---
+++ model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Normal
+
+class PPO(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(PPO, self).__init__()
+
+        self.actor = nn.Sequential(
+                nn.Linear(num_inputs, 256),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Linear(256, num_outputs)
+                )
+
+        self.critic = nn.Sequenetial(
+                nn.Linear(num_inputs, 256),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Linear(256, num_outputs)
+                )
+
+        self.std = nn.parameter(torch.ones(1, num_outputs))
+
+    def forward(self, x):
+        mu = self.actor(x)
+        std = self.std.expand_as(mu).exp()
+        dist = Normal(mu, std)
+        value = self.critic(x)
+
+        return dist, value
+                
